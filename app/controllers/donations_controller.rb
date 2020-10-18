46d66b48e9fb8eaccf1949793abe0e3566987c75
echigawa0921 <yuki.echigawa@di-v.co.jp>
2020-10-18 17:53:32 +0900

    formオブジェクトの実装完了

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,28 +1,25 @@
-class DonationsController < ApplicationController
+class  DonationsController < ApplicationController
   def index
   end
-
+ 
   def new
+    @user_donation = UserDonation.new   #「UserDonation」に編集
   end
-
+ 
   def create
-    user = User.create(user_params)
-    Address.create(address_params(user))
-    Donation.create(donation_params(user))
-    redirect_to action: :index
+    @user_donation = UserDonation.new(donation_params)   #「UserDonation」に編集
+     if @user_donation.valid?
+       @user_donation.save
+       redirect_to action: :index
+     else
+       render action: :new
+     end
   end
-
+ 
   private
-
-  def user_params
-    params.permit(:name, :name_reading, :nickname)
+   # 全てのストロングパラメーターを1つに統合
+  def donation_params
+   params.require(:user_donation).permit(:name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price)
   end
-
-  def address_params(user)
-    params.permit(:postal_code, :prefecture, :city, :house_number, :building_name).merge(user_id: user.id)
-  end
-
-  def donation_params(user)
-    params.permit(:price).merge(user_id: user.id)
-  end
-end
+ 
+ end
